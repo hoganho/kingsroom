f56367e3a421bbb1736e25cc1ada0df89e11e192
hoganho <hogan@bhhogroup.com>
2025-10-22 12:26:39 +1100

    feat: Initial project setup with React, Vite, and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-white">
+        Hello, Tailwind is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
