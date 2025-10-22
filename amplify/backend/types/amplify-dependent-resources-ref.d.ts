e92f4b6a0981647d96115ac39c9d6c551c3a74f0
hoganho <hogan@bhhogroup.com>
2025-10-22 13:52:50 +1100

    feat: Integrate Amplify Auth with default UI

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "kingsroomc05d6627": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
